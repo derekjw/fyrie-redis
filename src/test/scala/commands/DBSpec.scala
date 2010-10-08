78a4f73c69478867a162ae2f5113eedeb0dbe197
Derek Williams <derek@nebvin.ca>
2010-10-08 16:04:12 -0600

    Added test for new sort commands

--- src/test/scala/commands/DBSpec.scala
+++ src/test/scala/commands/DBSpec.scala
@@ -7,11 +7,10 @@ import org.scalatest.matchers.ShouldMatchers
 import org.scalatest.junit.JUnitRunner
 import org.junit.runner.RunWith
 
-
 @RunWith(classOf[JUnitRunner])
-class OperationsSpec extends Spec 
-                     with ShouldMatchers
-                     with RedisTestServer {
+class OperationsSpec extends Spec
+    with ShouldMatchers
+    with RedisTestServer {
 
   describe("keys") {
     it("should fetch keys") {
@@ -41,7 +40,7 @@ class OperationsSpec extends Spec
       r send set("anshin-2", "maulindu")
       r send rename("anshin-2", "anshin-2-new")
       val thrown = evaluating { r send rename("anshin-2", "anshin-2-new") } should produce[Exception]
-      thrown.getMessage should equal ("ERR no such key")
+      thrown.getMessage should equal("ERR no such key")
     }
   }
 
@@ -108,38 +107,49 @@ class OperationsSpec extends Spec
         dbsize,
         set("testkey2", "testvalue2"),
         dbsize,
-        mget(Seq("testkey1", "testkey2"))
-      )) should be(Result(List((), 1, (), 2, Result(List(Some("testvalue1"), Some("testvalue2"))))))
+        mget(Seq("testkey1", "testkey2")))) should be(Result(List((), 1, (), 2, Result(List(Some("testvalue1"), Some("testvalue2"))))))
     }
     it("should survive an error") {
       r send multiexec(Seq(
         set("a", "abc"),
         lpop("a"),
-        get("a")
-      )) match {
-        case Result(() :: (e: RedisErrorException) :: Result("abc") :: Nil) =>
+        get("a"))) match {
+        case Result(() ::(e: RedisErrorException) :: Result("abc") :: Nil) =>
           e.message should be("ERR Operation against a key holding the wrong kind of value")
         case NoResult => fail("Did not receive expected response")
       }
     }
   }
 
-  def mkList {
-    List(6, 3, 5, 47, 1, 1, 4, 9) foreach (r send lpush("sortlist", _))
-  }
-
-  def mkStringList {
-    List("lorem", "ipsum", "dolor", "sit", "amet", 3, 7) foreach (r send lpush("sortlist", _))
-  }
-
   describe("sort") {
     it("should do a simple sort") {
-      mkList
-      (r send sort("sortlist")).map(_.map(_.toInt)) should be(Result(List(1, 1, 3, 4, 5, 6, 9, 47)))
+      List(6, 3, 5, 47, 1, 1, 4, 9) foreach (r send lpush("sortlist", _))
+      (r send sort("sortlist")).map(_.flatMap(_.map(_.toInt))) should be(Result(List(1, 1, 3, 4, 5, 6, 9, 47)))
     }
     it("should do a lexical sort") {
-      mkStringList
-      r send sort[String]("sortlist", alpha = true) should be(Result(List("3", "7", "amet", "dolor", "ipsum", "lorem", "sit")))
+      List("lorem", "ipsum", "dolor", "sit", "amet", 3, 7) foreach (r send lpush("sortlist", _))
+      r send sort[String]("sortlist", alpha = true) map (_.flatten) should be(Result(List("3", "7", "amet", "dolor", "ipsum", "lorem", "sit")))
+    }
+    it("should return multiple items") {
+      import serialization.Parse.Implicits._
+      val list = List(("item1", "data1", 1, 4),
+                      ("item2", "data2", 2, 8),
+                      ("item3", "data3", 3, 1),
+                      ("item4", "data4", 4, 6),
+                      ("item5", "data5", 5, 3))
+      for ((key, data, num, rank) <- list) {
+        r send sadd("items", key)
+        r send set("data::"+key, data)
+        r send set("num::"+key, num)
+        r send set("rank::"+key, rank)
+      }
+      r send del(List("num::item1"))
+      r send sort3[String, String, Int]("items",
+                                        get = ("#", "data::*", "num::*"),
+                                        by = Some("rank::*"),
+                                        limit = Some((1,3))) should be(Result(List((Some("item5"), Some("data5"), Some(5)),
+                                                                                   (Some("item1"), Some("data1"), None),
+                                                                                   (Some("item4"), Some("data4"), Some(4)))))
     }
   }
 }
