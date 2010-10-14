7b58a196238f936666c25f3862618e0421d22f4e
Derek Williams <derek@nebvin.ca>
2010-10-13 22:28:10 -0600

    cleanup files

--- src/main/scala/RedisClient.scala
+++ src/main/scala/RedisClient.scala
@@ -1,4 +1,49 @@
-package net.fyrie.redis
+package net.fyrie
+package redis
+
+import actors.{RedisClientSession}
+import messages.{Request}
+
+import se.scalablesolutions.akka.dispatch.{Future, FutureTimeoutException}
+import se.scalablesolutions.akka.actor.{Actor,ActorRef}
+import Actor.{actorOf}
+import se.scalablesolutions.akka.dispatch._
+
+class RedisClient(address: String = "localhost", port: Int = 6379) {
+  val actor = actorOf(new RedisClientSession(address, port)).start
+
+  def ![A](command: Command[A])(implicit sender: Option[ActorRef] = None): Unit =
+    actor ! Request(command.toBytes, command.handler)
+
+  def !![A](command: Command[A]): Option[Result[A]] = {
+    val future = this !!! command
+    try {
+      future.await
+    } catch {
+      case e: FutureTimeoutException => None
+    }
+    if (future.exception.isDefined) throw future.exception.get
+    else future.result
+  }
+
+  def !!![A](command: Command[A]): Future[Result[A]] =
+    actor !!! Request(command.toBytes, command.handler)
+
+  def send[A](command: Command[A]): A = {
+    val future = this !!! command
+    future.await
+    if (future.exception.isDefined) throw future.exception.get
+    else future.result.get.get
+  }
+
+  def stop = actor.stop
+
+  def disconnect = stop
+}
+
+case class RedisErrorException(message: String) extends RuntimeException(message)
+case class RedisProtocolException(message: String) extends RuntimeException(message)
+case class RedisConnectionException(message: String) extends RuntimeException(message)
 
 trait SortOrder
 object SortOrder {
@@ -20,22 +65,3 @@ object AggregateScore {
     val getBytes = "MAX".getBytes
   }
 }
-
-/*
-trait Redis extends IO {
-  private val baseHandlers = new handlers.BaseHandlers
-
-  def send[T](cmd: Command[T]): T = {
-    writer.write(cmd.toBytes)
-    baseHandlers.forceLazyResults
-    cmd.handler(reader, baseHandlers)
-  }
-}
-
-class RedisClient(override val host: String, override val port: Int) extends Redis {
-  connect
-
-  def this() = this("localhost", 6379)
-  override def toString = host + ":" + String.valueOf(port)
-}
-*/
