120de5d7821e2c82cf8e4e0135370939feb00841
Derek Williams <derek@nebvin.ca>
2011-09-14 21:31:26 -0600

    formatting

--- src/test/scala/net/fyrie/redis/TestServer.scala
+++ src/test/scala/net/fyrie/redis/TestServer.scala
@@ -37,8 +37,8 @@ trait UnstableClient { self: mutable.Specification ⇒
 
     def around[T <% Result](t: ⇒ T) = {
       val result: Result = r.sync.info.lines.find(_ startsWith "redis_version").map(_.split(Array(':', '.')).tail.map(_.toInt).toList) match {
-        case Some(a :: b :: _) if (a > 2) || ((a == 2) && (b > 4)) => t
-        case _ => skipped
+        case Some(a :: b :: _) if (a > 2) || ((a == 2) && (b > 4)) ⇒ t
+        case _ ⇒ skipped
       }
       r.sync.flushall
       r.disconnect
