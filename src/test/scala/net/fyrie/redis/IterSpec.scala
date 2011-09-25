ed9c6e7b883fad7026ee34269daaf99bbb29b1ea
Derek Williams <derek@nebvin.ca>
2011-09-24 22:47:42 -0600

    Move Iteratee into IO

--- src/test/scala/net/fyrie/redis/IterSpec.scala
+++ src/test/scala/net/fyrie/redis/IterSpec.scala
@@ -11,7 +11,7 @@ class IterSpec extends Specification {
     val iter = for {
       a ← IO takeUntil ByteString(" ")
       b ← IO take 5
-      c ← IO.takeAll
+      c ← IO takeAll
     } yield (a.utf8String, b.utf8String, c.utf8String)
     iter(ByteString("Hel"))(ByteString("lo W"))(ByteString("orld!")) === IO.Done(("Hello", "World", "!"))
   }
