1f73271ec07e0f1556cff793336b0353b27601d6
Derek Williams <derek@nebvin.ca>
2011-09-26 06:57:11 -0600

    Update to sbt 0.11

--- project/Build.scala
+++ project/Build.scala
@@ -1,8 +1,8 @@
 import sbt._
 import Keys._
 
-import scalariform.formatter.preferences._
-import ScalariformPlugin.formatPreferences
+import com.typesafe.sbtscalariform.ScalariformPlugin
+import ScalariformPlugin.{ format, formatPreferences }
 
 object FyrieRedisBuild extends Build {
   lazy val core = Project("fyrie-redis",
@@ -24,17 +24,20 @@ object FyrieRedisBuild extends Build {
                                 "org.specs2" % "specs2-scalaz-core_2.9.1" % "6.0.1" % "test"),
     parallelExecution in Test := false,
 /*    formatPreferences in Compile := formattingPreferences,
-    formatPreferences in Test :=  formattingPreferences,  */
+    formatPreferences in Test :=  formattingPreferences, */
     publishTo <<= (version) { version: String =>
       val repo = (s: String) =>
         Resolver.ssh(s, "repo.fyrie.net", "/home/repo/" + s + "/") as("derek", file("/home/derek/.ssh/id_rsa")) withPermissions("0644")
       Some(if (version.trim.endsWith("SNAPSHOT")) repo("snapshots") else repo("releases"))
     })
 
-  val formattingPreferences = (FormattingPreferences()
-                               .setPreference(RewriteArrowSymbols, true)
-                               .setPreference(AlignParameters, true)
-                               .setPreference(AlignSingleLineCaseStatements, true))
+  val formattingPreferences = {
+    import scalariform.formatter.preferences._
+    FormattingPreferences()
+    .setPreference(RewriteArrowSymbols, true)
+    .setPreference(AlignParameters, true)
+    .setPreference(AlignSingleLineCaseStatements, true)
+  }
 
 }
 
---
+++ project/plugins.sbt
@@ -0,0 +1,3 @@
+resolvers += "Typesafe Repo" at "http://repo.typesafe.com/typesafe/releases/"
+
+addSbtPlugin("com.typesafe.sbtscalariform" % "sbt-scalariform" % "0.1.4")
